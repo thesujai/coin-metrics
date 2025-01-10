647c9494e3519818fef7b8b050173b1a580d3ca8
Sujai Kumar Gupta <sujayscience1234@gmail.com>
2025-01-10 21:39:51 +0530

    feat: Refactor deviation and stats controllers to use input validation

--- src/controllers/deviations.controller.js
+++ src/controllers/deviations.controller.js
@@ -1,30 +1,24 @@
-const Deviation = require("../models/deviation.model")
-const { SUPPORTED_COINS } = require("../constants")
+const Deviation = require('../models/deviation.model');
+const validateAndFetchCoinData = require('./validator');
 
 const getDeviation = async (req, res) => {
     try {
-
         const { coin } = req.params;
-        if (!coin)
-            return res.status(400).json({ error: "Query param 'coin' is required" });
 
-        if (!SUPPORTED_COINS.includes(coin)) {
-            return res.status(400).json({ error: "Unsupported coin" });
-        }
-
-        const deviationData = await Deviation.findOne({ coin });
-
-        if (!deviationData) {
-            return res.status(404).json({ error: "Deviation data not found" });
-        }
+        const deviationData = await validateAndFetchCoinData(
+            coin,
+            Deviation,
+            res,
+        );
+        if (!deviationData) return;
 
         return res.json({
             deviation: deviationData.deviation,
         });
     } catch (err) {
-        console.error("Error fetching deviation data:", err.message);
-        return res.status(500).json({ error: "An error occurred" });
+        console.error('Error fetching deviation data:', err.message);
+        return res.status(500).json({ error: 'An error occurred' });
     }
-}
+};
 
 module.exports = getDeviation;
--- src/controllers/stats.controller.js
+++ src/controllers/stats.controller.js
@@ -1,26 +1,22 @@
 const Crypto = require('../models/crypto.model');
-const { SUPPORTED_COINS} = require("../constants")
+const validateAndFetchCoinData = require('./validator');
 
 const getStats = async (req, res) => {
-    const { coin } = req.params;
-    if (!coin)
-        return res
-            .status(400)
-            .json({ error: "Query param 'coin' is required" });
-    
-    if (!SUPPORTED_COINS.includes(coin)) {
-        return res.status(400).json({ error: "Unsupported coin" });
+    try {
+        const { coin } = req.params;
+
+        const latest = await validateAndFetchCoinData(coin, Crypto, res);
+        if (!latest) return;
+
+        return res.json({
+            price: latest.price,
+            marketCap: latest.marketCap,
+            '24hChange': latest.change24h,
+        });
+    } catch (err) {
+        console.error('Error fetching stats data:', err.message);
+        return res.status(500).json({ error: 'An error occurred' });
     }
-
-    // as timestamp is indexed, the query wont take a lot of time
-    const latest = await Crypto.findOne({ coin }).sort({ createdAt: -1 });
-    if (!latest) return res.status(404).json({ error: 'Data not found' });
-
-    res.json({
-        price: latest.price,
-        marketCap: latest.marketCap,
-        '24hChange': latest.change24h,
-    });
 };
 
 module.exports = getStats;
