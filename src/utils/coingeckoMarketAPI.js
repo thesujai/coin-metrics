4d3440129bd054c51d07598eab87643ceed09d8b
Sujai Kumar Gupta <sujayscience1234@gmail.com>
2025-01-10 14:51:17 +0530

    feat: Add Coingecko market API utility function

---
+++ src/utils/coingeckoMarketAPI.js
@@ -0,0 +1,32 @@
+const axios = require("axios");
+const { SUPPORTED_COINS } = require("../constants");
+
+/**
+ * @param {Array} data - The raw cryptocurrency data
+ * @returns {Array} Serialized cryptocurrency data.
+ */
+const serializeCryptoData = (data) => {
+  return data.map((coin) => ({
+    coinId: coin.id,
+    price: coin.current_price,
+    marketCap: coin.market_cap,
+    change24h: coin.price_change_percentage_24h,
+  }));
+};
+
+const fetchCoingeckoMarkets = async () => {
+  const coins = SUPPORTED_COINS;
+  const url = "https://api.coingecko.com/api/v3/coins/markets";
+  const params = { vs_currency: "usd", ids: coins.join(",") };
+
+  try {
+    const response = await axios.get(url, { params });
+    return serializeCryptoData(response.data);
+  } catch (error) {
+    console.error("Error fetching data from CoinGecko:", error.message);
+    // we should never stop the process because of an error at the CoinGecko API level, so let it continue
+    return [];
+  }
+};
+
+module.exports = fetchCoingeckoMarkets;
