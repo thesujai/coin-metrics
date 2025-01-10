18ccee63c2ed70b04023c4168c19dcb88fb49caa
Sujai Kumar Gupta <sujayscience1234@gmail.com>
2025-01-10 14:27:42 +0530

    feat: Add enum validation for coin field in Crypto model

--- src/models/crypto.model.js
+++ src/models/crypto.model.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 const cryptoSchema = new mongoose.Schema({
-  coin: { type: String, required: true },
+  coin: {
+    type: String,
+    required: true,
+    enum: ["bitcoin", "ethereum", "matic-network"],
+  },
   price: { type: Number, required: true },
   marketCap: { type: Number, required: true },
   change24h: { type: Number, required: true },
